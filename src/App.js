241320b9635ea3738e6ffdd6b56e3804a5a823e6
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-07-28 15:55:58 -0500

    add pages/router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
     return (
-        <div>
-            Hi
-        </div>
+        <Router>
+            <Route exact path="/" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+        </Router>
     );
 }
 

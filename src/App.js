89459a219f1f46b889df797bbb719b201a043e1e
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-07-29 16:42:06 -0500

    add header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import Header from "./components/Header";
 
 function App() {
     return (
         <Router>
+            <Header />
             <Route exact path="/" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/contact" component={Contact} />
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="flex flex-row">
+            <h1 className="flex-auto text-center text-3xl font-semibold p-4">Daniel Moody-Huhn</h1>
+            <h1 className="flex-auto text-center text-3xl font-semibold p-4">☰</h1>
+        </header>
+    );
+}
+
+export default Header;

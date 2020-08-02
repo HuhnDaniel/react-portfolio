9f7e5a296ed8b82a6975804052af4a92793923eb
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-08-02 18:33:27 -0500

    fix routing

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             <Switch>
                 <Route exact path="/portfolio" component={Portfolio} />
                 <Route exact path="/contact" component={Contact} />
-                <Route exact path="*" component={About} />
+                <Route exact path="/react-portfolio/*" component={About} />
             </Switch>
         </Router>
     );
